861a36ec3c75f4fe7268c186bcee8834a9f82aed
tartarin <m.tartarin@hotmail.fr>
2022-08-01 15:59:09 +0200

    changement route

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,15 @@ const path = require('path');
 
 const app = express();
 
+const users = require('./route/user')
+
 app.use(bodyParser.json());
 const corsOption ={
                origin: '*',
 };
 
 app.use(cors(corsOption));
+
+app.use('/api/newsletter', users)
 
 module.exports = app;
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,5 +4,14 @@ exports.signup = (req,res,next)=>{
 
 }
 exports.login = (req, res, next)=>{
-               
+    let data = {
+          prenom : req.body.prenom,
+          nom : req.body.nom,
+          mail : req.body.mail
+    } 
+    db.query(`INSERT INTO newsletter SET ?`, data, (err, rows)=>{
+       if(err){
+               return res.status(500).json({message: err.message})
+       } return res.status(200).json({message: "utilisateur crée"})
+    })          
 }
